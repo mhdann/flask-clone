6092afda1421ae5dc2a1c77c6232b116f1df1a3e
Michael Dann <mhdann@gmail.com>
2016-03-14 22:52:23 -0400

    Bug Check 4

--- plots.py
+++ plots.py
@@ -2,6 +2,13 @@ from bokeh.plotting import *
 from bokeh.plotting import figure
 from bokeh.charts import Line
 from bokeh.embed import components 
+import numpy as np
+
+def datetime(x):
+  # Converte to numpy datetime explicitly
+  # http://bokeh.pydata.org/en/latest/docs/gallery/stocks.html
+  return np.array(x, dtype=np.datetime64)
+
 
 # Define a function that will return an HTML snippet. Cribbed:
 # https://github.com/rpazyaquian/bokeh-flask-tutorial/blob/master/app/plots.py
@@ -10,12 +17,19 @@ def build_plot(data, ticker):
   print "build_plot() reached"
   print "Data: "
   print data
+  p = figure(x_axis_type = 'datetime', width=800, height=800)
+  p.title = ticker 
+  p.xaxis.axis_label = 'Date'
 
-  if 'High' in data.columns:
+  if 'Close' in data.columns:
     # construct the plot
-    p = Line(data.High, title=ticker, xlabel='Date', ylabel='Price', width=800, height=800)
-  elif 'VALUE' in data.columns:
-    p = Line(data.VALUE, title=ticker, xlabel='Date', ylabel='Price', width=800, height=800)  
+   p.line(datetime(data.Date), data.Close)
+   p.yaxis.axis_label = 'Price'
+
+  else:
+    col = 0
+    p.yaxis.axis_label = data.columns[col]
+    p.line(datetime(data.index), data.iloc[:,col])
 
   print "Plot made"
 
